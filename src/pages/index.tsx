9018551cd207fbe1ea2bd33a48564fd52ca86b63
charlie <charlieward18@gmail.com>
2023-04-11 09:13:28 +0100

    added the ability to view other posts on the index page

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,7 +1,7 @@
 import { type NextPage } from "next";
 import Head from "next/head";
-import { SignInButton, SignOutButton, useUser } from "@clerk/nextjs";
-import { api } from "~/utils/api";
+import { SignInButton, useUser } from "@clerk/nextjs";
+import { type RouterOutputs, api } from "~/utils/api";
 
 const CreatePostsWizard = () => {
   const { user } = useUser();
@@ -9,13 +9,37 @@ const CreatePostsWizard = () => {
   if (!user) return null;
 
   return (
-    <div className="flex gap-4">
+    <div className="flex w-full gap-3">
       <img
         src={user.profileImageUrl}
         alt="profile image"
         className="h-14 w-14 rounded-full"
       />
-      <input placeholder="type some emojis" className="bg-transparent" />
+      <input
+        placeholder="type some emojis"
+        className="grow bg-transparent outline-none"
+      />
+    </div>
+  );
+};
+
+type PostWithUser = RouterOutputs["posts"]["getAll"][number];
+
+const PostView = (props: PostWithUser) => {
+  const { post, author } = props;
+  return (
+    <div key={post.id} className="flex gap-3 border-b border-slate-400 p-4">
+      <img
+        src={author.profileImageUrl}
+        alt="profile image"
+        className="h-14 w-14 rounded-full"
+      />
+      <div className="flex flex-col">
+        <div className="flex text-slate-300">
+          <span>{`@${author.username}`}</span>
+        </div>
+        <span>{post.content}</span>
+      </div>
     </div>
   );
 };
@@ -49,10 +73,8 @@ const Home: NextPage = () => {
             {user.isSignedIn && <CreatePostsWizard />}
           </div>
           <div className="flex flex-col">
-            {data?.map((post) => (
-              <div key={post.id} className="border-b border-slate-400 p-8">
-                {post.content}
-              </div>
+            {data?.map((fullPost) => (
+              <PostView {...fullPost} key={fullPost.post.id} />
             ))}
           </div>
         </div>
@@ -62,27 +84,3 @@ const Home: NextPage = () => {
 };
 
 export default Home;
-
-// const AuthShowcase: React.FC = () => {
-//   const { data: sessionData } = useSession();
-
-//   const { data: secretMessage } = api.example.getSecretMessage.useQuery(
-//     undefined, // no input
-//     { enabled: sessionData?.user !== undefined },
-//   );
-
-//   return (
-//     <div className="flex flex-col items-center justify-center gap-4">
-//       <p className="text-center text-2xl text-white">
-//         {sessionData && <span>Logged in as {sessionData.user?.name}</span>}
-//         {secretMessage && <span> - {secretMessage}</span>}
-//       </p>
-//       <button
-//         className="rounded-full bg-white/10 px-10 py-3 font-semibold text-white no-underline transition hover:bg-white/20"
-//         onClick={sessionData ? () => void signOut() : () => void signIn()}
-//       >
-//         {sessionData ? "Sign out" : "Sign in"}
-//       </button>
-//     </div>
-//   );
-// };
--- src/server/api/routers/posts.ts
+++ src/server/api/routers/posts.ts
@@ -1,10 +1,49 @@
+import type { User } from "@clerk/nextjs/dist/api";
+import { clerkClient } from "@clerk/nextjs/server";
+import { TRPCError } from "@trpc/server";
 import {
   createTRPCRouter,
   publicProcedure,
 } from "~/server/api/trpc";
 
+const filterUsersForClient = (user: User) => {
+    return {
+        id: user.id,
+        username: user.username,
+        profileImageUrl: user.profileImageUrl
+    }
+}
+
 export const postsRouter = createTRPCRouter({
-  getAll: publicProcedure.query(({ ctx }) => {
-    return ctx.prisma.post.findMany();
+  getAll: publicProcedure.query(async ({ ctx }) => {
+    const posts = await ctx.prisma.post.findMany({
+        take: 100
+    });
+
+    const users = (
+        await clerkClient.users.getUserList({
+            userId: posts.map((post) => post.authorId),
+            limit: 100
+        })).map(filterUsersForClient)
+
+    return posts.map((post) => {
+        const author = users.find(user => user.id === post.authorId)
+
+        // console.log(author)
+
+        if(!author || !author.username) throw new TRPCError({
+            code: 'INTERNAL_SERVER_ERROR',
+            message: 'Author not found'
+        })
+        
+        return {
+            post, 
+            author: {
+                ...author, 
+                username: author.username
+            }
+        }
+        }
+    )
   }),
 });
