8d99d4330902eb7917495abee5e883f284284260
Charlie Ward <charlieward18@gmail.com>
2023-04-12 19:54:24 +0100

    added toast notifications for bad emoji input

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -4,10 +4,12 @@ import { api } from "~/utils/api";
 
 import "~/styles/globals.css";
 import { ClerkProvider } from "@clerk/nextjs";
+import { Toaster } from "react-hot-toast";
 
 function MyApp({ Component, pageProps }: AppProps) {
   return (
     <ClerkProvider {...pageProps}>
+      <Toaster position={"bottom-center"} />
       <Component {...pageProps} />
     </ClerkProvider>
   );
--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -5,8 +5,9 @@ import { type RouterOutputs, api } from "~/utils/api";
 import dayjs from "dayjs";
 import relativeTime from "dayjs/plugin/relativeTime";
 import Image from "next/image";
-import { LoadingPage } from "~/components/loadingSpinner";
+import { LoadingSpinner, LoadingPage } from "~/components/loadingSpinner";
 import { useState } from "react";
+import { toast } from "react-hot-toast";
 
 dayjs.extend(relativeTime);
 
@@ -21,6 +22,14 @@ const CreatePostsWizard = () => {
     onSuccess: () => {
       setInput("");
       void ctx.posts.getAll.invalidate();
+    },
+    onError: (e) => {
+      const fieldErrors = e.data?.zodError?.fieldErrors.content;
+      if (fieldErrors && fieldErrors[0]) {
+        toast.error(fieldErrors[0]);
+      } else {
+        toast.error("Failed to post. Please try again later.");
+      }
     },
   });
 
@@ -39,16 +48,33 @@ const CreatePostsWizard = () => {
         value={input}
         onChange={(e) => setInput(e.target.value)}
         placeholder="type some emojis"
+        onKeyDown={(e) => {
+          if (e.key === 'Enter') {
+            e.preventDefault()
+            if(input !== '') {
+              mutate({ content: input });
+            }
+          }
+        }}
         disabled={isPosting}
         className="grow bg-transparent outline-none"
       />
-      <button
-        onClick={() => {
-          mutate({ content: input });
-        }}
-      >
-        Post
-      </button>
+      {input !== "" && !isPosting && (
+        <button
+          disabled={isPosting}
+          onClick={() => {
+            mutate({ content: input });
+          }}
+        >
+          Post
+        </button>
+      )}
+
+      {isPosting && (
+        <div className="flex items-center justify-center">
+          <LoadingSpinner size={25} />
+        </div>
+      )}
     </div>
   );
 };
